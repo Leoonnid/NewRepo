37778d75372daa125f43c6d809bcc352065f244d
Leoonnid <limonchic32@gmail.com>
2025-04-30 12:27:32 +0300

    Выполнен расчет F

--- P5/Program.cs
+++ P5/Program.cs
@@ -13,6 +13,7 @@ namespace P5
             Console.WriteLine("Введите значение параметра x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+            double F = 2 * Math.Cos(x);
         }
     }
 }
